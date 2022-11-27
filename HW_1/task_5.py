aa1828028534996dcd9ab4566a8d9d584e8f12de
Karina <Karinanifontova06@mail.ru>
2022-11-27 15:27:22 +0300

    HW_1/task_5

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -11,3 +11,13 @@
 11 5
 9.22
 """
+import math
+
+print('Введите координаты точек')
+a = int(input('x1 = '))
+b = int(input('y1 = '))
+c = int(input('x2 = '))
+d = int(input('y2 = '))
+
+e = abs(math.sqrt((c - a) ** 2 + (d - b) ** 2))
+print(f'Расстояние между точками ({a};{b}) и ({c};{d}) равно {round(e, 2)}')
