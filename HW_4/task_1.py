4f541757d82cb52d1c992340bd3a6db1ec4fc488
Karina <Karinanifontova06@mail.ru>
2022-12-04 21:29:15 +0300

    HW_4/task_1

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,25 @@
 38
 [2, 19]
 """
+
+num = int(input('Введите натуральное число: '))
+new_list = []
+
+sieve = [el for el in range(num + 1)]
+sieve[1] = 0
+sieve_list = []
+index = 2
+while index <= num:
+    if sieve[index] != 0:
+        sieve_list.append(sieve[index])
+        for i in range(0, num + 1, index):
+            sieve[i] = 0
+    index += 1
+
+while num != 1:
+    for k in range(len(sieve_list + 1)):
+        if num % sieve_list[k] == 0:
+            new_list.append(sieve_list[k])
+            num /= sieve_list[k]
+
+print(new_list)
