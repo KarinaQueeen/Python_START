ea540da717dce1dc1b9f1cd17f01f88095ad0f89
Karina <Karinanifontova06@mail.ru>
2022-12-26 21:46:10 +0300

    HW_5/task_3

--- HW_5/task_3.py
+++ HW_5/task_3.py
@@ -1,3 +1,54 @@
 """
 Напишите игру "Крестики-нолики".
 """
+from random import randint
+
+def print_board(list):
+    print('-' * 13)
+    for el in list:
+        print('|', end=' ')
+        print(*el, sep=' | ', end=' | \n')
+        print('-' * 13)
+
+def check(list):
+    if list[0][0] == list[0][1] == list[0][-1] != ' ':
+        return 1
+    elif list[1][0] == list[1][1] == list[1][-1] != ' ':
+        return 1
+    elif list[-1][0] == list[-1][1] == list[-1][-1] != ' ':
+        return 1
+    elif list[0][0] == list[1][0] == list[-1][0] != ' ':
+        return 1
+    elif list[0][1] == list[1][1] == list[-1][1] != ' ':
+        return 1
+    elif list[0][-1] == list[1][-1] == list[-1][-1] != ' ':
+        return 1
+    elif list[0][0] == list[1][1] == list[-1][-1] != ' ':
+        return 1
+    elif list[0][-1] == list[1][1] == list[-1][0] != ' ':
+        return 1
+    else:
+        return 0
+
+if __name__ == '__main__':
+    lst = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
+print('КРЕСТИКИ-НОЛИКИ')
+print_board(lst)
+
+players = ['O', 'X']
+turn = randint(0, 1)
+player = players[turn]
+
+while True:
+    print(f'Укажите расположение {player}: строку и столбец через пробел.')
+    row, col = [int(i) for i in input().split()]
+    lst[row][col] = player
+    c = check(lst)
+    if c == 1:
+        print('Победил ', player)
+        break
+    else:
+        turn = not turn
+        player = players[turn]
+        print_board(lst)
