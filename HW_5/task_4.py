1b101e8b04de2c0244acc740ee715ee74dd10467
Karina <Karinanifontova06@mail.ru>
2022-12-26 23:11:00 +0300

    HW_5/task_4

--- HW_5/task_4.py
+++ HW_5/task_4.py
@@ -7,6 +7,37 @@
 Вывод: значение типа <str>, можно записать в файл.
 
 Примеры:
-ыыыыыррррр   аааааагггггггг
+
 5ы5р3 6а8г
 """
+def coding(line):
+    code = ''
+    count = 1
+    symbol = ''
+
+    for s in line:
+        if s != symbol:
+            if symbol:
+                code += str(count) + symbol
+            count = 1
+            symbol = s
+        else:
+            if count == 9:
+                code += str(count) + symbol
+                count = 1
+            count += 1
+    return code
+
+
+def decoding(code):
+    decode = ''
+    for i in range(0, len(code), 2):
+        count, simbol = code[i: i + 1]
+        decode += simbol * int(count)
+    return decode
+
+line = input('Введите строку для шифрования: ')
+code = coding(line)
+
+print(code)
+print(decoding(code))
