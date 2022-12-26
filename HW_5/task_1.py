ebee57e55d81250b5820332ccec40690e9a19c35
Karina <Karinanifontova06@mail.ru>
2022-12-26 20:02:47 +0300

    HW_5/task_1

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,15 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+text = input('Введите текст:\n').lower().split()
+
+def del_word_1(text):
+    return ' '.join(el for el in text if not ('а' in el and 'б' in el and 'в' in el))
+
+
+def del_word_2(text):
+    return ' '.join(filter(lambda x: not ('а' in x and 'б' in x and 'в' in x), text))
+
+print(del_word_1(text))
+print(del_word_2(text))
