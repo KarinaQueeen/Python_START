0fbe50672972f4e85cc5cdb863f6d1450053b8cd
Karina <Karinanifontova06@mail.ru>
2022-12-26 21:23:52 +0300

    HW_5/task_2

--- HW_5/task_2.py
+++ HW_5/task_2.py
@@ -9,3 +9,55 @@
 a) Добавьте игру против бота
 b) Подумайте, как наделить бота простейшим "интеллектом"
 """
+from random import randint
+
+sweets = int(input('Введите общее количество конфет: '))
+max_sweets = int(input('Укажите максимальное количество конфет за ход: '))
+
+start = sweets % (max_sweets + 1)
+#print('Выигрышный первый ход', start)
+
+players = ['Бот', 'Игрок']
+turn = randint(0, 1)
+player = players[turn]
+
+while True:
+    if sweets == max_sweets:
+        print()
+        print('Победил', player)
+        break
+    else:
+        print('-' * 10)
+        print('Ходит', player)
+        if player == players[1]:
+            g = int(input('Возьмите конфеты: '))
+            sweets -= g
+            turn = not turn
+            player = players[turn]
+            if sweets <= max_sweets:
+                print()
+                print('Победил', player)
+                break
+            else:
+                print('Осталось:', sweets, 'шт')
+                f = randint(1, max_sweets)
+                print(player, ' взял ', f, 'шт')
+                sweets -= f
+                turn = not turn
+                player = players[turn]
+                if sweets <= max_sweets:
+                    print()
+                    print('Победил', player)
+                    break
+                print('Осталось:', sweets, 'шт')
+        else:
+            f = randint(1, max_sweets)
+            print(player, ' взял ', f, 'шт')
+            sweets -= f
+            turn = not turn
+            player = players[turn]
+            if sweets <= max_sweets:
+                print()
+                print('Победил', player)
+                break
+            print('Осталось:', sweets, 'шт')
